cc8228cb0a96ede734d460b8daf3aa317d0c52f2
Sumon Hossain <sumonhosainrakib@hotmail.com>
2022-08-10 16:27:47 +0600

    Update videos.js
    Hi Lama,  Your video.js file needs some changes. I'm one of your Youtube students. Thank you for this great tutorial

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { addVideo, addView, getByTag, getVideo, random, search, sub, trend } from "../controllers/video.js";
+import { addVideo,updateVideo,deleteVideo, addView, getByTag, getVideo, random, search, sub, trend } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
 //create a video
 router.post("/", verifyToken, addVideo)
-router.put("/:id", verifyToken, addVideo)
-router.delete("/:id", verifyToken, addVideo)
+router.put("/:id", verifyToken, updateVideo)
+router.delete("/:id", verifyToken, deleteVideo)
 router.get("/find/:id", getVideo)
 router.put("/view/:id", addView)
 router.get("/trend", trend)
