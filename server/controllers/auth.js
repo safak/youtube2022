de69210287bc69bda16c7eea7ed3b6b220b71fde
Sumon Hossain <sumonhosainrakib@hotmail.com>
2022-09-08 06:48:30 +0600

    Update auth.js
    Hi Safak. " await " has no effect on the type of this expression.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,7 +22,7 @@ export const signin = async (req, res, next) => {
     const user = await User.findOne({ name: req.body.name });
     if (!user) return next(createError(404, "User not found!"));
 
-    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    const isCorrect =  bcrypt.compare(req.body.password, user.password);
 
     if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
 
