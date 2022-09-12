2d31b56d073ab48a567b9ce9df6556747726cfe9
Sumon Hossain <sumonhosainrakib@hotmail.com>
2022-09-12 08:02:48 +0600

    Update Video.jsx
    There is some typo error on Iframe

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -114,7 +114,7 @@ const Video = () => {
             height="720"
             src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
