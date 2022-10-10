6c8e7724f2eccbb04b18f6c90da61db2052bb42b
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-10-10 20:21:42 +0530

    verifyUser
    Removed next from line number 18

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -15,7 +15,7 @@ export const verifyToken = (req, res, next) => {
 };
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, () => {
+  verifyToken(req, res,  () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
