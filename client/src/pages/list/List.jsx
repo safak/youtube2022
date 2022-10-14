d75112f1952ccf6f721def94e977d7e54e70c958
Mrunal Tupe <65716766+MRUNAL727@users.noreply.github.com>
2022-10-14 10:05:24 +0530

    input

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -35,7 +35,7 @@ const List = () => {
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
               <label>Destination</label>
-              <input placeholder={destination} type="text" />
+              <input placeholder={destination} type="text" onChange={(event)=>setDestination(event.target.value)}/>
             </div>
             <div className="lsItem">
               <label>Check-in Date</label>
