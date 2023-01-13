03ec284e747139fb4fc2211435f38b2141d2abc3
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.ac.in>
2023-01-13 16:08:11 +0530

    Update Featured.jsx
    Kindly check the edited lines, as the proper methods for rendering array elements in place of count of hotel by cities

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -20,7 +20,7 @@ const Featured = () => {
             />
             <div className="featuredTitles">
               <h1>Berlin</h1>
-              <h2>{data[0]} properties</h2>
+              <h2>{data.data[0]} properties</h2>
             </div>
           </div>
 
@@ -32,7 +32,7 @@ const Featured = () => {
             />
             <div className="featuredTitles">
               <h1>Madrid</h1>
-              <h2>{data[1]} properties</h2>
+              <h2>{data.data[1]} properties</h2>
             </div>
           </div>
           <div className="featuredItem">
@@ -43,7 +43,7 @@ const Featured = () => {
             />
             <div className="featuredTitles">
               <h1>London</h1>
-              <h2>{data[2]} properties</h2>
+              <h2>{data.data[2]} properties</h2>
             </div>
           </div>
         </>
