2be033c7a3a1d06d3813fc809e47d0077652e88e
Emilio <87607608+Made-in-Heaven-dotcom@users.noreply.github.com>
2023-06-26 18:00:31 -0400

    Comment timestamp added

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,39 +1,40 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { format } from "timeago.js";
 
 const Container = styled.div`
-  display: flex;
-  gap: 10px;
-  margin: 30px 0px;
+    display: flex;
+    gap: 10px;
+    margin: 30px 0px;
 `;
 
 const Avatar = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
 `;
 
 const Details = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  color: ${({ theme }) => theme.text};
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    color: ${({ theme }) => theme.text};
 `;
 const Name = styled.span`
-  font-size: 13px;
-  font-weight: 500;
+    font-size: 13px;
+    font-weight: 500;
 `;
 
 const Date = styled.span`
-  font-size: 12px;
-  font-weight: 400;
-  color: ${({ theme }) => theme.textSoft};
-  margin-left: 5px;
+    font-size: 12px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.textSoft};
+    margin-left: 5px;
 `;
 
 const Text = styled.span`
-  font-size: 14px;
+    font-size: 14px;
 `;
 
 const Comment = ({ comment }) => {
@@ -41,8 +42,8 @@ const Comment = ({ comment }) => {
 
   useEffect(() => {
     const fetchComment = async () => {
-      const res = await axios.get(`/users/find/${comment.userId}`);
-      setChannel(res.data)
+      const res = await axios.get(`/api/users/find/${comment.userId}`);
+      setChannel(res.data);
     };
     fetchComment();
   }, [comment.userId]);
@@ -52,7 +53,7 @@ const Comment = ({ comment }) => {
       <Avatar src={channel.img} />
       <Details>
         <Name>
-          {channel.name} <Date>1 day ago</Date>
+          {channel.name} <Date>{format(comment.createdAt)}</Date>
         </Name>
         <Text>{comment.desc}</Text>
       </Details>
