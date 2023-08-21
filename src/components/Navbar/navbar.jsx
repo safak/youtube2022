917b2a0e14b24cbbdffd29991ac48286d297f4ab
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-08-21 21:16:12 +0300

    Navbar

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from 'react-router-dom';
+import "./navbar.scss";
+import { useSelector } from "react-redux"; // Import useSelector
+import { useState } from 'react';
+import Cart from "../Cart/Cart";
+
+const Navbar = () => {
+  // Use useSelector to get the 'products' state from Redux
+  const products = useSelector((state) => state.cart.products);
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+        </div>
+        <div className="center">
+          <Link className="link" to="/">Home Life</Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">Homepage</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon" onClick={() => setOpen(!open)}>
+              <ShoppingCartOutlinedIcon />
+              {/* Use the 'products' variable to display the cart count */}
+              <span>{products.length}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {open && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
