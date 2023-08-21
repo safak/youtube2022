f5f2180cd4a7c2023fc3f199c4dc996978f8ba01
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-08-21 22:36:00 +0300

    Request

---
+++ src/makeRequest.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const makeRequest = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+  },
+});
