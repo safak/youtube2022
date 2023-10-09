103e378559aa32d528ae0d71520439a201421ef6
Liam <liamc214@hotmail.com>
2023-10-09 15:06:06 +0100

    .js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
