8de3fcc2eb954d5a4b84568b0177a2ffbe557600
Liam <liamc214@hotmail.com>
2023-10-09 14:50:24 +0100

    a

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Register = () => {
   return (
     <div className='formContainer'>
-     
      <div className='formWrapper'>
         <form action=""></form>
      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
