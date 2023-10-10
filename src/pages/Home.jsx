5a21fe5facb59ac56f5446c8aca607bce789b29e
Liam <liamc214@hotmail.com>
2023-10-10 09:44:19 +0100

    SideBar+Chat

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className="container">
+            <Sidebar/>  
+            <Chat/>
+        </div>
+    </div>
   )
 }
 
