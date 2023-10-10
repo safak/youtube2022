c2c21dd20195bd6c10c100d634e9b5611ee6b90a
Liam <liamc214@hotmail.com>
2023-10-10 09:49:53 +0100

    TestingHome*failed*

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss"
 
 function App() {
   return (
-    <Login/>
+    <Home/>
     );
 }
 
