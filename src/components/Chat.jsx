30a8e82e88fef2d510d9c16bb844dc22a37125a9
Liam <liamc214@hotmail.com>
2023-10-10 10:06:12 +0100

    Adjustments

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className='Chat'>Chat</div>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>Sidebar</div>
   )
 }
 
