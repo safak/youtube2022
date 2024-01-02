b6d9753f515e44302b9107771775c8ddd2c0f351
spshah1701 <142957290+spshah1701@users.noreply.github.com>
2024-01-02 01:04:57 -0700

    Update Datatable.jsx
    When the code was const [list, setList] = useState(); it created an error as length of initial state i.e. undefined was accessed.
    
    const [list, setList] = useState([]);  initializes the list state variable with an empty array and provides a function setList to update the state. This solves the issues, as now length can be accessed.

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 const Datatable = ({columns}) => {
   const location = useLocation();
   const path = location.pathname.split("/")[1];
-  const [list, setList] = useState();
+  const [list, setList] = useState([]);
   const { data, loading, error } = useFetch(`/${path}`);
 
   useEffect(() => {
