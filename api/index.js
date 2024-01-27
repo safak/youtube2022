881ad24bd23127396af8e399ce4a5b82a60810fd
James Sode <ayindejam2009@yahoo.com>
2024-01-27 12:06:08 +0000

    added app get route in api index js file

--- api/index.js
+++ api/index.js
@@ -46,6 +46,10 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/relationships", relationshipRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!"); // You can customize this message
+});
+
 app.listen(8800, () => {
   console.log("API working!");
 });
